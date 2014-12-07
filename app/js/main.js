5ab9f8e1413ef415604f6610541cfd77f6cb97a1
David Smith <davidsmith2@gmail.com>
2014-12-07 16:08:56 -0500

    tidy up naming conventions

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 angular
-    .module('bai', ['mgo-angular-wizard'])
-    .factory('symptoms', [function () {
-        var responses = [
+    .module('BAI', ['mgo-angular-wizard'])
+    .factory('ResponsesService', [function () {
+        var responses, API;
+        responses = [
             {
                 rating: 0,
                 description: 'Not at all'
@@ -19,65 +20,80 @@ angular
                 description: 'A lot'
             }
         ];
-        var o = {
-            collection: [
-                {
-                    _id: '1',
-                    description: 'Anxiety, nervousness, worry, or fear',
-                    responses: responses,
-                    category: 'Anxious Feelings'
-                },
-                {
-                    _id: '2',
-                    description: 'Feeling that things around you are strange, unreal, or foggy',
-                    responses: responses,
-                    category: 'Anxious Feelings'
-                },
-                {
-                    _id: '3',
-                    description: 'Feeling detached from all or part of your body',
-                    responses: responses,
-                    category: 'Anxious Feelings'
-                },
-                {
-                    _id: '4',
-                    description: 'Difficulty concentrating',
-                    responses: responses,
-                    category: 'Anxious Thoughts'
-                },
-                {
-                    _id: '5',
-                    description: 'Racing thoughts or having your mind jump from one thing to the next',
-                    responses: responses,
-                    category: 'Anxious Thoughts'
-                },
-                {
-                    _id: '6',
-                    description: 'Frightening fantasies or daydreams',
-                    responses: responses,
-                    category: 'Anxious Thoughts'
-                },
-                {
-                    _id: '7',
-                    description: 'Skipping or racing or pounding of the heart (sometimes called "palpitations")',
-                    responses: responses,
-                    category: 'Physical Symptoms'
-                },
-                {
-                    _id: '8',
-                    description: 'Pain, pressure, or tightness in the chest',
-                    responses: responses,
-                    category: 'Physical Symptoms'
-                },
-                {
-                    _id: '9',
-                    description: 'Tingling or numbness in the toes or fingers',
-                    responses: responses,
-                    category: 'Physical Symptoms'
-                }
-            ],
-            getCategories: function () {
-                var array = o.collection,
+        API = {
+            list: function () {
+                return responses;
+            }
+        };
+        return API;
+    }])
+    .factory('SymptomsService', ['ResponsesService', function (ResponsesService) {
+        var symptoms, API;
+        symptoms = [
+            {
+                _id: '1',
+                description: 'Anxiety, nervousness, worry, or fear',
+                responses: ResponsesService.list(),
+                category: 'Anxious Feelings'
+            },
+            {
+                _id: '2',
+                description: 'Feeling that things around you are strange, unreal, or foggy',
+                responses: ResponsesService.list(),
+                category: 'Anxious Feelings'
+            },
+            {
+                _id: '3',
+                description: 'Feeling detached from all or part of your body',
+                responses: ResponsesService.list(),
+                category: 'Anxious Feelings'
+            },
+            {
+                _id: '4',
+                description: 'Difficulty concentrating',
+                responses: ResponsesService.list(),
+                category: 'Anxious Thoughts'
+            },
+            {
+                _id: '5',
+                description: 'Racing thoughts or having your mind jump from one thing to the next',
+                responses: ResponsesService.list(),
+                category: 'Anxious Thoughts'
+            },
+            {
+                _id: '6',
+                description: 'Frightening fantasies or daydreams',
+                responses: ResponsesService.list(),
+                category: 'Anxious Thoughts'
+            },
+            {
+                _id: '7',
+                description: 'Skipping or racing or pounding of the heart (sometimes called "palpitations")',
+                responses: ResponsesService.list(),
+                category: 'Physical Symptoms'
+            },
+            {
+                _id: '8',
+                description: 'Pain, pressure, or tightness in the chest',
+                responses: ResponsesService.list(),
+                category: 'Physical Symptoms'
+            },
+            {
+                _id: '9',
+                description: 'Tingling or numbness in the toes or fingers',
+                responses: ResponsesService.list(),
+                category: 'Physical Symptoms'
+            }
+        ];
+        API = {
+            list: function () {
+                return symptoms;
+            },
+            listByCategory: function (category) {
+                return _.where(symptoms, {category: category});
+            },
+            listCategories: function () {
+                var array = symptoms,
                     unique = {},
                     distinct = [];
                 for (var i in array) {
@@ -87,25 +103,23 @@ angular
                     unique[array[i].category] = 0;
                 }
                 return distinct;
-            },
-            getSymptomsByCategory: function (category) {
-                return _.where(o.collection, {category: category});
             }
         };
-        return o;
+        return API;
     }])
-    .factory('inventory', [function () {
-        var o = {
-            model: {
-                date: new Date(),
-                responses: []
-            },
+    .factory('InventoryService', [function () {
+        var inventory, API;
+        inventory = {
+            date: new Date(),
+            responses: []
+        };
+        API = {
             addResponse: function (response) {
-                o.model.responses.push(response);
+                inventory.responses.push(response);
             },
             getScore: function () {
                 var score = 0,
-                    responses = o.model.responses,
+                    responses = inventory.responses,
                     i;
                 for (i = 0; i < responses.length; i++) {
                     score += parseInt(responses[i], 10);
@@ -130,21 +144,21 @@ angular
                 return degree;
             }
         };
-        return o;
+        return API;
     }])
-    .controller('WizardMainCtrl', ['symptoms', 'inventory', function (symptoms) {
+    .controller('MainCtrl', ['SymptomsService', function (SymptomsService) {
         var self = this;
-        self.categories = symptoms.getCategories();
+        self.categories = SymptomsService.listCategories();
         self.isFinished = false;
         self.onFinish = function () {
             self.isFinished = true;
         };
     }])
-    .controller('WizardSubCtrl', ['symptoms', 'WizardHandler', function (symptoms, WizardHandler) {
+    .controller('SubCtrl', ['SymptomsService', 'WizardHandler', function (SymptomsService, WizardHandler) {
         var self = this;
         self.mainWizard = WizardHandler.wizard('mainWizard');
         self.getSymptoms = function (category) {
-            return symptoms.getSymptomsByCategory(category);
+            return SymptomsService.listByCategory(category);
         };
         self.onFinish = function (isLastCategory) {
             if (!isLastCategory) {
@@ -154,20 +168,20 @@ angular
             }
         };
     }])
-    .controller('FormCtrl', ['WizardHandler', 'inventory', function (WizardHandler, inventory) {
+    .controller('FormCtrl', ['InventoryService', 'WizardHandler', function (InventoryService, WizardHandler) {
         var self = this;
         self.submit = function (categoryIndex) {
-            inventory.addResponse(self.inventory.response);
+            InventoryService.addResponse(self.inventory.response);
             self.subWizard = WizardHandler.wizard('subWizard' + categoryIndex);
             self.subWizard.next();
         };
     }])
-    .controller('ScoreboardCtrl', ['inventory', function (inventory) {
+    .controller('ScoreboardCtrl', ['InventoryService', function (InventoryService) {
         var self = this;
         self.getScore = function () {
-            return inventory.getScore();
+            return InventoryService.getScore();
         };
         self.getDegree = function () {
-            return inventory.getDegree(inventory.getScore());
+            return InventoryService.getDegree(InventoryService.getScore());
         };
     }]);
