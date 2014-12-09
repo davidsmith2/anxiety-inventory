677d538e280291b83cc89007673704438de5685a
davidsmith2 <davidsmith2@gmail.com>
2014-12-08 22:18:11 -0500

    stand up express

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
-
 var connect = require('gulp-connect');
+var server = require('gulp-express');
 
 gulp.task('connect', function () {
     connect.server({
         root: 'app',
         port: 8888
+    });
+});
+
+gulp.task('server', function () {
+    server.run({
+        file: 'server/app.js'
     });
 });
---
+++ server/app.js
@@ -0,0 +1,30 @@
+var ejs = require('ejs'),
+    express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    app;
+
+app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8888);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, '../app')));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Burns Anxiety Inventory'
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
